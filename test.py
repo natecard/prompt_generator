3921bb696bc99b8fd2cb9dcd915ec11d9d179cab
Nate Card <ncard24@gmail.com>
2024-04-01 23:02:24 -0400

    Refactor imports and update function names

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 import streamlit as st
-from langchain_community.llms import Ollama
+from langchain_community.llms.ollama import Ollama
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage, AIMessage
-from langchain_community.document_loaders import WebBaseLoader
-from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.document_loaders.web_base import WebBaseLoader
+from langchain_community.embeddings.ollama import OllamaEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain, create_history_aware_retriever
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
 
 embeddings = OllamaEmbeddings()
 
@@ -18,13 +19,16 @@ def vectorstore_url(url):
 
     text_splitter = RecursiveCharacterTextSplitter()
     chunks = text_splitter.split_documents(docs)
-    vector = FAISS.from_documents(chunks, embeddings)
+
+    vector = FAISS.add_documents(chunks, embeddings)
+
     return vector
 
 
-def context_retriever(vector_store):
-    retriever = vector_store.as_retriever()
+def context_retriever_chain(vector_store):
     llm = Ollama(model="llama2")
+
+    retriever = vector_store.as_retriever()
 
     prompt = ChatPromptTemplate.from_messages(
         [
@@ -36,29 +40,36 @@ def context_retriever(vector_store):
             ),
         ]
     )
+
     retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+
     return retriever_chain
 
 
-def rag_chain(retriever_chain):
+def get_rag_chain(retriever_chain):
     llm = Ollama(model="llama2")
+
     prompt = ChatPromptTemplate.from_messages(
         [
             (
                 "system",
-                "Answer the above conversaion, generate a search query to look up to get information relevant to the conversation",
-            )
+                "Answer the user's questions based on the below context:\n\n{context}",
+            ),
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("user", "{input}"),
         ]
     )
+
     document_chain = create_stuff_documents_chain(llm, prompt)
+
     return create_retrieval_chain(retriever_chain, document_chain)
 
 
-def response(input):
-    retriever_chain = context_retriever(st.session_state.vector_store)
+def get_response(user_input):
+    retriever_chain = context_retriever_chain(st.session_state.vector_store)
     conversation_chain = create_stuff_documents_chain(retriever_chain)
     response = conversation_chain.invoke(
-        {"chat history": st.session_state.chat_history, "input": input}
+        {"chat history": st.session_state.chat_history, "input": user_input}
     )
     return response["answer"]
 
@@ -79,8 +90,9 @@ with st.sidebar:
         if "vector_store" not in st.session_state:
             st.session_state.vector_store = vectorstore_url(url)
 
-        user_query = st.chat_input("User", placeholder="Type your message here")
+        user_query = st.chat_input("Type your message here")
         if user_query is not None and user_query != "":
+            response = get_response(user_query)
             st.session_state.chat_history.append(HumanMessage(content=user_query))
             st.session_state.chat_history.append(AIMessage(content=response))
 
