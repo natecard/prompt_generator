0c6da95f240940afcb089ae1285fe5ba47ceb198
Nate Card <ncard24@gmail.com>
2024-04-02 22:48:11 -0400

    Refactor code to improve modularity

--- test.py
+++ test.py
@@ -1,33 +1,38 @@
 import streamlit as st
-from langchain_community.llms.ollama import Ollama
+from langchain_community.chat_models import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage, AIMessage
-from langchain_community.document_loaders.web_base import WebBaseLoader
-from langchain_community.embeddings.ollama import OllamaEmbeddings
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains.retrieval import create_retrieval_chain
-from langchain.chains.history_aware_retriever import create_history_aware_retriever
-
-embeddings = OllamaEmbeddings()
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from transformers import AutoTokenizer
 
 
-def vectorstore_url(url):
+def get_loader(url):
     loader = WebBaseLoader(url)
     docs = loader.load()
-
-    text_splitter = RecursiveCharacterTextSplitter()
-    chunks = text_splitter.split_documents(docs)
-
-    vector = FAISS.add_documents(chunks, embeddings)
-
-    return vector
+    return docs
 
 
-def context_retriever_chain(vector_store):
-    llm = Ollama(model="llama2")
+def chunk_text(text):
+    splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
+        AutoTokenizer.from_pretrained("gpt2"), chunk_size=400, chunk_overlap=40
+    )
+    chunks = splitter.split_documents(text)
+    return chunks
 
+
+def get_vector_store(chunks):
+    embeddings = OllamaEmbeddings()
+    vector_store = FAISS.from_documents(chunks, embeddings)
+    return vector_store
+
+
+def get_context_retriever(vector_store):
+    llm = ChatOllama(model="llama2")
     retriever = vector_store.as_retriever()
 
     prompt = ChatPromptTemplate.from_messages(
@@ -40,35 +45,30 @@ def context_retriever_chain(vector_store):
             ),
         ]
     )
-
     retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
-
     return retriever_chain
 
 
-def get_rag_chain(retriever_chain):
-    llm = Ollama(model="llama2")
-
+def get_conversation_rag_chain(retriever_chain):
+    llm = ChatOllama(model="llama2")
     prompt = ChatPromptTemplate.from_messages(
         [
             (
                 "system",
-                "Answer the user's questions based on the below context:\n\n{context}",
+                "Answer the above conversaion, generate a search query to look up to get information relevant to the conversation",
             ),
             MessagesPlaceholder(variable_name="chat_history"),
             ("user", "{input}"),
         ]
     )
-
     document_chain = create_stuff_documents_chain(llm, prompt)
-
     return create_retrieval_chain(retriever_chain, document_chain)
 
 
 def get_response(user_input):
-    retriever_chain = context_retriever_chain(st.session_state.vector_store)
-    conversation_chain = create_stuff_documents_chain(retriever_chain)
-    response = conversation_chain.invoke(
+    retriever_chain = get_context_retriever(st.session_state.vector_store)
+    conversation_rag_chain = create_stuff_documents_chain(retriever_chain)
+    response = conversation_rag_chain.invoke(
         {"chat history": st.session_state.chat_history, "input": user_input}
     )
     return response["answer"]
@@ -85,10 +85,12 @@ with st.sidebar:
     else:
         if "chat_history" not in st.session_state:
             st.session_state.chat_history = [
-                AIMessage(content="Hello, how can I help you today?")
+                AIMessage(content="Hello, I am a bot. How can I help you?"),
             ]
         if "vector_store" not in st.session_state:
-            st.session_state.vector_store = vectorstore_url(url)
+            st.session_state.vector_store = get_vector_store(
+                chunk_text(get_loader(url))
+            )
 
         user_query = st.chat_input("Type your message here")
         if user_query is not None and user_query != "":
