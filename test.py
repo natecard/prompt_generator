b7988b3325447033bc30a6bfbe67537da28ac8b5
Nate Card <ncard24@gmail.com>
2024-04-03 16:03:21 -0400

    Add Streamlit chat functionality and update dependencies

--- test.py
+++ test.py
@@ -14,15 +14,20 @@ from langchain.chains import create_history_aware_retriever, create_retrieval_ch
 from transformers import AutoTokenizer
 from langchain.agents import AgentExecutor, ConversationalChatAgent
 from langchain.tools import Tool
+from clear_results import with_clear_container
 from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
 
-msgs = StreamlitChatMessageHistory()
-memory = ConversationBufferMemory(
-    chat_memory=msgs,
-    return_messages=True,
-    memory_key="chat_history",
-    output_key="output",
-)
+# Set up memory
+msgs = StreamlitChatMessageHistory(key="chat_message_history")
+if len(msgs.messages) == 0:
+    msgs.add_ai_message("How can I help you?")
+
+st.set_page_config(page_title="RAG Docs Chatbot", page_icon="🔗")
+st.title("RAG Docs Chatbot")
+
+view_messages = st.expander("View the message contents in session state")
+llm = Ollama(model="llama2")
 
 
 def get_loader(url):
@@ -46,37 +51,42 @@ def get_vector_store(chunks):
 
 
 def get_context_retriever(vector_store):
-    llm = Ollama(model="llama2")
+    # llm = Ollama(model="llama2")
     retriever = vector_store.as_retriever()
-
-    prompt = ChatPromptTemplate.from_messages(
+    contextualize_q_system_prompt = """Given a chat history and the latest user question \
+    which might reference context in the chat history, formulate a standalone question \
+    which can be understood without the chat history. Do NOT answer the question, \
+    just reformulate it if needed and otherwise return it as is."""
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
         [
-            MessagesPlaceholder(variable_name="chat_history"),
-            ("user", "{input}"),
-            (
-                "user",
-                "Given the above conversation, generate a search query to look up to get information relevant to the conversation",
-            ),
-        ]
-    )
-    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
-    return retriever_chain
-
-
-def get_conversation_rag_chain(retriever_chain):
-    llm = Ollama(model="llama2")
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                "Answer the above conversation, generate a search query to look up to get information relevant to the conversation:\n\n{context}",
-            ),
+            ("system", contextualize_q_system_prompt),
             MessagesPlaceholder(variable_name="chat_history"),
             ("user", "{input}"),
         ]
     )
-    document_chain = create_stuff_documents_chain(llm, prompt)
-    return create_retrieval_chain(retriever_chain, document_chain)
+    history_aware_retriever_chain = create_history_aware_retriever(
+        llm, retriever, contextualize_q_prompt
+    )
+    return history_aware_retriever_chain
+
+
+def get_conversation_rag_chain(history_aware_retriever_chain):
+    # llm = Ollama(model="llama2")
+
+    qa_system_prompt = """You are an assistant for question-answering and retrieval augmented tasks. \
+        Use the following pieces of retrieved context to answer the question. \
+        If you don't know the answer, just say that you don't know.\n\n\
+        {context}"""
+
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", qa_system_prompt),
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("user", "{input}"),
+        ]
+    )
+    document_chain = create_stuff_documents_chain(llm, qa_prompt)
+    return create_retrieval_chain(history_aware_retriever_chain, document_chain)
 
 
 def get_response(user_input):
@@ -94,15 +104,29 @@ def get_response(user_input):
     return response["answer"]
 
 
-def get_agent_response(user_input):
-    rag_tool = Tool(
+msgs = StreamlitChatMessageHistory()
+memory = ConversationBufferMemory(
+    chat_memory=msgs,
+    return_messages=True,
+    memory_key="chat_history",
+    output_key="output",
+)
+search = DuckDuckGoSearchAPIWrapper()
+tools = [
+    Tool(
+        name="search",
+        func=search.run,
+        description="Use the DuckDuckGo search engine to find information",
+    ),
+    Tool(
         name="rag",
         func=get_response,
         description="Use the RAG model to retrieve relevant information from the URL",
-    )
+    ),
+]
 
-    llm = Ollama(model="llama2", streaming=True)
-    tools = [DuckDuckGoSearchRun(name="search"), rag_tool]
+
+def get_agent_response(user_input):
     chat_agent = ConversationalChatAgent.from_llm_and_tools(llm=llm, tools=tools)
 
     agent = AgentExecutor.from_agent_and_tools(
@@ -115,22 +139,24 @@ def get_agent_response(user_input):
         handle_parsing_errors=True,
     )
 
-    response = agent.run(
-        {"input": user_input, "chat_history": st.session_state.chat_history}
-    )
-    return response
+    result = agent.invoke(input=user_input, context=memory.chat_memory)
+
+    if "output" in result:
+        return result["output"]
+    else:
+        return "\n".join(result["intermediate_steps"])
 
 
-st.set_page_config(page_title="RAG Docs Chatbot", page_icon="🔗")
-st.title("RAG Docs Chatbot")
 chat_container = st.container()
-
-with chat_container:
+with st.sidebar:
     st.header("Setup")
     url = st.text_input("Enter URL to load documents")
     if url is None or url == "":
         st.info("Please enter a URL to load documents")
-    else:
+
+with chat_container:
+    st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+    if url is not None and url != "":
         if "chat_history" not in st.session_state:
             st.session_state.chat_history = [
                 AIMessage(content="Hello, I am a bot. How can I help you?"),
@@ -140,16 +166,28 @@ with chat_container:
                 chunk_text(get_loader(url))
             )
 
-        user_query = st.chat_input("Type your message here")
-        if user_query is not None and user_query != "":
-            response = get_agent_response(user_query)
-            st.session_state.chat_history.append(HumanMessage(content=user_query))
-            st.session_state.chat_history.append(AIMessage(content=response))
+    user_query = st.chat_input("Type your message here")
+    if user_query is not None and user_query != "":
+        response = get_agent_response(user_query)
+        st.session_state.chat_history.append(HumanMessage(content=user_query))
+        st.session_state.chat_history.append(AIMessage(content=response))
 
-        for message in st.session_state.chat_history:
-            if isinstance(message, AIMessage):
-                with st.chat_message("AI"):
-                    st.write(message.content)
-            elif isinstance(message, HumanMessage):
-                with st.chat_message("Human"):
-                    st.write(message.content)
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message("AI"):
+                st.write(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("Human"):
+                st.write(message.content)
+
+    # Render current messages from StreamlitChatMessageHistory
+    for msg in msgs.messages:
+        st.chat_message(msg.type).write(msg.content)
+
+    # If user inputs a new prompt, generate and draw a new response
+    if prompt := st.chat_input():
+        st.chat_message("human").write(prompt)
+        # Note: new messages are saved to history automatically by Langchain during run
+        config = {"configurable": {"session_id": "any"}}
+        response = get_agent_response.invoke(user_query)
+        st.chat_message("ai").write(response.content)
